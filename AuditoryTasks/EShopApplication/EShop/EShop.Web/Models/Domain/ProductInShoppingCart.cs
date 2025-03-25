713531a6e85a3272f1ca1d6371922648bc8a3816
pupinoskii <Ivan Pupinoski>
2025-03-25 04:18:50 +0100

    E-Shop Application!

---
+++ AuditoryTasks/EShopApplication/EShop/EShop.Web/Models/Domain/ProductInShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿namespace EShop.Web.Models.Domain
+{
+    public class ProductInShoppingCart
+    {
+        // public Guid ID { get; set; } // surrogate key
+        public Guid ProductID { get; set; }
+        public Product? Product { get; set; }
+        public Guid ShoppingCartID { get; set; }
+        public ShoppingCart? ShoppingCart { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ AuditoryTasks/EShopApplication/EShop/EShop.Web/Models/Domain/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using EShop.Web.Models.Identity;
+
+namespace EShop.Web.Models.Domain
+{
+    public class ShoppingCart
+    {
+        public Guid ID { get; set; }
+        public string? OwnerID { get; set; }
+        public EShopApplicationUser? Owner { get; set; }
+        public virtual ICollection<ProductInShoppingCart> ProductInShoppingCarts { get; set; }
+    }
+}
