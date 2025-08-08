0fea15009221a17f5065c3d32bf964bb67c5bf42
pupinoskii <Ivan Pupinoski>
2025-08-08 03:31:22 +0200

    EShop App!

---
+++ AuditoryTasks/EShopApplication/EShop/EShop.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using EShop.Web.Models.Domain;
+using EShop.Web.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Web.Data;
+
+public class ApplicationDbContext : IdentityDbContext<EShopApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public virtual DbSet<ProductInShoppingCart> ProductInShoppingCarts { get; set; }
+
+    // fluent api
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Product>()
+            .Property(z => z.ID)
+            .ValueGeneratedOnAdd();
+
+        builder.Entity<ShoppingCart>()
+            .Property(z => z.ID)
+            .ValueGeneratedOnAdd();
+
+        builder.Entity<ProductInShoppingCart>()
+            .HasKey(z => new { z.ProductID, z.ShoppingCartID });
+
+        builder.Entity<ProductInShoppingCart>()
+            .HasOne(z => z.Product)
+            .WithMany(z => z.ProductInShoppingCarts)
+            .HasForeignKey(z => z.ShoppingCartID);
+
+        builder.Entity<ProductInShoppingCart>()
+            .HasOne(z => z.ShoppingCart)
+            .WithMany(z => z.ProductInShoppingCarts)
+            .HasForeignKey(z => z.ProductID);
+
+        builder.Entity<ShoppingCart>()
+            .HasOne<EShopApplicationUser>(z => z.Owner)
+            .WithOne(z => z.UserCart)
+            .HasForeignKey<ShoppingCart>(z => z.OwnerID);
+        
+        /* [Required] Annotation
+        builder.Entity<Product>()
+            .Property(z => z.ProductName)
+            .IsRequired();
+        */
+    }
+}
---
+++ AuditoryTasks/EShopApplication/EShop/EShop.Web/Data/Migrations/20250321001035_AddingModelsWithFluentAPI.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingModelsWithFluentAPI : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductPrice = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShoppingCarts",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OwnerID = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCarts", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCarts_AspNetUsers_OwnerID",
+                        column: x => x.OwnerID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductInShoppingCarts",
+                columns: table => new
+                {
+                    ProductID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ShoppingCartID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductInShoppingCarts", x => new { x.ProductID, x.ShoppingCartID });
+                    table.ForeignKey(
+                        name: "FK_ProductInShoppingCarts_Products_ShoppingCartID",
+                        column: x => x.ShoppingCartID,
+                        principalTable: "Products",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductInShoppingCarts_ShoppingCarts_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "ShoppingCarts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductInShoppingCarts_ShoppingCartID",
+                table: "ProductInShoppingCarts",
+                column: "ShoppingCartID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCarts_OwnerID",
+                table: "ShoppingCarts",
+                column: "OwnerID",
+                unique: true,
+                filter: "[OwnerID] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductInShoppingCarts");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingCarts");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
