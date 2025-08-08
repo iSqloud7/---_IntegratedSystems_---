d2f5078de8ce6131800703adad016cb8d76d3ddf
pupinoskii <Ivan Pupinoski>
2025-08-08 02:52:32 +0200

    Lab01!

---
+++ LaboratoryTasks/#01 AthletesApplication/Athletes.Domain/Models/Domain/Participation.cs
@@ -0,0 +1,21 @@
+﻿namespace Athletes.Domain.Models.Domain
+{
+    public class Participation
+    {
+        public Guid ID { get; set; }
+
+        public DateOnly DateRegistered { get; set; }
+
+        public int Result { get; set; }
+
+        public string? Performance { get; set; }
+
+        public Guid CompetitionID { get; set; }
+
+        public virtual Competition? Competition { get; set; }
+
+        public Guid AthleteID { get; set; }
+
+        public virtual Athlete? Athlete { get; set; }
+    }
+}
